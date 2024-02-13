991ec3b95f2e2d92be3d573050b48258afd2ddf0
SSU0515 <kimsh159357@naver.com>
2024-02-14 01:50:46 +0900

    240213

---
+++ 240213/script.js
@@ -0,0 +1,68 @@
+// const button = document.querySelector("button");
+// button.addEventListener("click", () => {
+//   // alert("오늘도 화이팅입니당:)");
+//   // confirm("오늘도 화이팅 하실건지?");
+//   // prompt("너의 이름은");
+// });
+
+// const name = "쑤";
+// window.document.write("제 이름은" + name + "입니다.");
+
+// var num;
+// num = 7;
+
+// let num = 7;
+// document.write(num);
+
+// const str = "hello world";
+
+// let str01 = "life is cool";
+
+// str01 = "life is good";
+
+// console.log(str01);
+
+// node ./script.js
+
+// const str = "test";
+// console.log(str);
+
+// const data = 5;
+// const str = "hello";
+// const boolean = true;
+
+const newBook = {
+  title: "최재천의 곤충사회",
+  price: 16300,
+  delivery: "free",
+  author: "최재천",
+  publishingDate: "2024-02-13",
+};
+
+console.log(newBook.title);
+const bookTitle = newBook.title;
+document.write(bookTitle);
+
+// const arr = [1,2,3,4,5];
+// const newBook01 = ["최재천의 곤충사회",16300,"free","최재천","2024-02-13"];
+
+// const result = Number(prompt("당신의 나이를 입력하세요"));
+// console.log(typeof result);
+// const tenYear = result + 10;
+// console.log("당신은 10년뒤" + tenYear + "세 입니다.");
+// console.log(`당신의 10년뒤 나이는 ${tenYear}세 입니다`);
+
+// const num = "10";
+// console.log(typeof num);
+
+// const result = prompt("당신의 나이를 입력하세요");
+// console.log(result);
+
+// const result = confirm("오늘 점심 뭐먹지?");
+// if (result === true) {
+//   alert("맛있게 드세여");
+// } else {
+//   alert("why");
+// }
+
+// console.log(result);
